93e82d35991bbe939274f852a5460537c9b87363
wnkinc <140204064+wnkinc@users.noreply.github.com>
2024-11-30 19:15:28 -0800

    Complete step 10

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require("path");
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -20,9 +22,41 @@ const messages = [
     added: new Date(),
   },
 ];
+const links = [
+  { href: "/", text: "Home" },
+  { href: "new", text: "New" },
+];
 
 app.get("/", (req, res) => {
-  res.render("index", { title: "Mini Messageboard", messages: messages });
+  res.render("index", {
+    title: "Mini Messageboard",
+    messages: messages,
+    links: links,
+  });
+});
+
+app.get("/new", (req, res) => {
+  res.render("form", {
+    links: links,
+  });
+});
+
+// Handle the form submission (POST request)
+app.post("/new", (req, res) => {
+  // Get data from the form submission
+  const { name, message } = req.body;
+
+  // Add new message to the messages array
+  const newMessage = {
+    text: message,
+    user: name,
+    added: new Date(),
+  };
+
+  messages.push(newMessage); // Push the new message to the array
+
+  // Redirect back to the main page to display the updated messages
+  res.redirect("/"); // Redirect to the home page
 });
 
 const PORT = process.env.PORT || 3000;
