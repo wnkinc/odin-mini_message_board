159137d6be845685dd459bcd63eba488ffa4eacc
wnkinc <140204064+wnkinc@users.noreply.github.com>
2024-12-06 21:49:26 -0800

    add folders and few basic files

--- app.js
+++ app.js
@@ -1,76 +1,79 @@
 const express = require("express");
 const app = express();
-const path = require("path");
+const messageRouter = require("./routes/messageRouter");
+// const path = require("path");
 
-const assetsPath = path.join(__dirname, "public");
-app.use(express.static(assetsPath));
+// const assetsPath = path.join(__dirname, "public");
+// app.use(express.static(assetsPath));
 
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.use("/", messageRouter);
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-];
-const links = [
-  { href: "/", text: "Home" },
-  { href: "/new", text: "New" },
-];
+// app.set("views", path.join(__dirname, "views"));
 
-app.get("/", (req, res) => {
-  res.render("index", {
-    title: "Mini Messageboard",
-    messages: messages,
-    links: links,
-  });
-});
+// const messages = [
+//   {
+//     text: "Hi there!",
+//     user: "Amando",
+//     added: new Date(),
+//   },
+//   {
+//     text: "Hello World!",
+//     user: "Charles",
+//     added: new Date(),
+//   },
+// ];
+// const links = [
+//   { href: "/", text: "Home" },
+//   { href: "/new", text: "New" },
+// ];
 
-app.get("/new", (req, res) => {
-  res.render("form", {
-    links: links,
-  });
-});
+// app.get("/", (req, res) => {
+//   res.render("index", {
+//     title: "Mini Messageboard",
+//     messages: messages,
+//     links: links,
+//   });
+// });
 
-// Handle the form submission (POST request)
-app.post("/new", (req, res) => {
-  // Get data from the form submission
-  const { name, message } = req.body;
+// app.get("/new", (req, res) => {
+//   res.render("form", {
+//     links: links,
+//   });
+// });
 
-  // Add new message to the messages array
-  const newMessage = {
-    text: message,
-    user: name,
-    added: new Date(),
-  };
+// // Handle the form submission (POST request)
+// app.post("/new", (req, res) => {
+//   // Get data from the form submission
+//   const { name, message } = req.body;
 
-  messages.push(newMessage); // Push the new message to the array
+//   // Add new message to the messages array
+//   const newMessage = {
+//     text: message,
+//     user: name,
+//     added: new Date(),
+//   };
 
-  // Redirect back to the main page to display the updated messages
-  res.redirect("/"); // Redirect to the home page
-});
+//   messages.push(newMessage); // Push the new message to the array
 
-// message details page
-app.get("/messages/:id", (req, res) => {
-  const messageId = req.params.id;
-  console.log("Message ID:", messageId); // Log the ID to see what you're getting
-  const message = messages[messageId];
+//   // Redirect back to the main page to display the updated messages
+//   res.redirect("/"); // Redirect to the home page
+// });
 
-  if (message) {
-    res.render("messageDetails", { message: message, links: links });
-  } else {
-    res.status(404).send("Message not found");
-  }
-});
+// // message details page
+// app.get("/messages/:id", (req, res) => {
+//   const messageId = req.params.id;
+//   console.log("Message ID:", messageId); // Log the ID to see what you're getting
+//   const message = messages[messageId];
+
+//   if (message) {
+//     res.render("messageDetails", { message: message, links: links });
+//   } else {
+//     res.status(404).send("Message not found");
+//   }
+// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
